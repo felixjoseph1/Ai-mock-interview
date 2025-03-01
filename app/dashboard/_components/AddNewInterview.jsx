fa3ac2670c6e4b4ac3a9a2d02e3a63f6d5d6cb05
felix <felixjoseph2003@gmail.com>
2025-03-01 16:45:30 +0530

    Questions generated through Gemini API

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -9,9 +9,35 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { chatSession } from "@/utils/geminiModal";
+import { LoaderCircle } from "lucide-react";
+import { json } from "drizzle-orm/gel-core";
 
 const AddNewInterview = () => {
   const [openDialog, setOpenDialog] = useState(false);
+  const [jobPosition, setJobPosition] = useState();
+  const [jobDescription, setJobDescription] = useState();
+  const [jobExperience, setJobExperience] = useState();
+  const [loading, Setloading] = useState(false);
+
+  const onSubmit = async (e) => {
+    Setloading(true);
+    e.preventDefault();
+    console.log(jobDescription, jobPosition, jobExperience);
+    //const InputPrompt = `job position: ${jobPosition}, job description: ${jobDescription}, years of experience: ${jobExperience}. based on the above details. generate 5 interview questions with answers in json format`;
+    const InputPrompt = `job position: ${jobPosition}, job description: ${jobDescription}, years of experience: ${jobExperience}.based on the above details, generate 5 interview questions with answers in json format. the answers should be easily parsable through JSON.parse()`;
+    const result = await chatSession.sendMessage(InputPrompt);
+    const JsonResponse = result.response
+      .text()
+      .replace("```json", "")
+      .replace("```", "");
+    console.log(JsonResponse);
+    console.log(JSON.parse(JsonResponse));
+    Setloading(false);
+  };
   return (
     <div>
       <div
@@ -22,14 +48,72 @@ const AddNewInterview = () => {
       >
         <h2 className="text-lg  text-center"> + Add New</h2>
       </div>
+
       <Dialog open={openDialog} onOpenChange={setOpenDialog}>
-        <DialogContent>
+        <DialogContent className={`!max-w-2xl`}>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle className={`text-2xl`}>
+              Tell us more about the job you are Interviewing
+            </DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              Add details about your job position/role , Job description and
+              years of experience
             </DialogDescription>
+            <form onSubmit={onSubmit}>
+              <div className="mt-5 my-5">
+                <label>Job role/Job Position</label>
+                <Input
+                  placeholder="Ex. Full Stack Developer"
+                  required
+                  onChange={(event) => {
+                    setJobPosition(event.target.value);
+                  }}
+                />
+              </div>
+              <div className=" my-5">
+                <label>Job Description/Tech Stack (In Short)</label>
+                <Textarea
+                  placeholder="Ex. React, NodeJs, MongoDB, MySQL"
+                  required
+                  onChange={(event) => {
+                    setJobDescription(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="my-5">
+                <label>Years of Experience</label>
+                <Input
+                  type="number"
+                  placeholder="Ex. 2"
+                  max="40"
+                  required
+                  onChange={(event) => {
+                    setJobExperience(event.target.value);
+                  }}
+                />
+              </div>
+              <div className="flex gap-5 justify-end">
+                <Button
+                  onClick={() => {
+                    setOpenDialog(false);
+                  }}
+                  variant="ghost"
+                  className="focus-visible:ring-0 focus-visible:ring-offset-0  hover:text-red-700"
+                >
+                  Cancel
+                </Button>
+                <Button type="submit">
+                  {loading ? (
+                    <>
+                      <LoaderCircle className="animate-spin" />
+                      Generating from AI
+                    </>
+                  ) : (
+                    "Start Interview"
+                  )}
+                </Button>
+              </div>
+            </form>
           </DialogHeader>
         </DialogContent>
       </Dialog>
